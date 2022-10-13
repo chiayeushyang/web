3e97048b0b18f78a61af5e5085603119caa04bd8
Chia Yeu Shyang <yeushyang020825@gmail.com>
2022-10-13 18:17:11 +0800

    homework2 done

--- week3/date_array.php
+++ week3/date_array.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-    <title>Week3_Homework 1</title>
+    <title>Week3_Homework 2</title>
 </head>
 
 <body>
@@ -28,8 +28,11 @@
             <select class="form-select form-select-lg mb-3 bg-warning " aria-label="Date">
                 <option selected>MONTH</option>
                 <?php
+                $months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
                 for ($num = 1; $num <= 12; $num++) {
-                    echo "<option value=\"$num\">$num</option>";
+                    $month = $months[$num -1];
+                    echo "<option value=\"$num\">$month</option>";
                 }
                 ?>
             </select>
