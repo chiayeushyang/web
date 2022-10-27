9cbe6dc88a78496854e76e930d5315adf9beef40
Chia Yeu Shyang <yeushyang020825@gmail.com>
2022-10-27 18:34:53 +0800

    add product read php

---
+++ project/product_read.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Products</h1>
+        </div>
+
+        <!-- PHP code to read records will be here -->
+        <?php
+        // include database connection
+        include 'config/database.php';
+
+        // delete message prompt will be here
+
+        // select all data
+        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+
+        // link to create record form
+        echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Name</th>";
+            echo "<th>Description</th>";
+            echo "<th>Price</th>";
+            echo "<th>Action</th>";
+            echo "</tr>";
+
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$id}</td>";
+                echo "<td>{$name}</td>";
+                echo "<td>{$description}</td>";
+                echo "<td>{$price}</td>";
+                echo "<td>";
+                // read one record
+                echo "<a href='product_read_one.php?id={$id}' class='btn btn-info me-1'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='product_update.php?id={$id}' class='btn btn-primary me-1'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_product({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+            // end table
+            echo "</table>";
+        } else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+</body>
+
+</html>
